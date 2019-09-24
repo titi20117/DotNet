699a25223e98016e310f1a2148f3b6f67461c2a2
Fernand <moudio.fernand@yandex.ru>
2019-09-24 05:49:32 +0300

    Task1 done

--- Moudio_Fernand_Task02/Task1/Program.cs
+++ Moudio_Fernand_Task02/Task1/Program.cs
@@ -10,6 +10,41 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            Console.Write("Напишите значению сторону a прямоугольника a = ");
+            string sideA = Console.ReadLine();
+            Console.Write("Напишите значению сторону b прямоугольника b = ");
+            string sideB = Console.ReadLine();
+            int area = FindAreaRectangle(sideA, sideB);
+            Console.WriteLine("Площадь прямоугольника S = " + area);
+        }
+
+        static int FindAreaRectangle (string sideA, string sideB)
+        {
+            int area = 0;
+            int sideNameA, sideNameB;
+            if (Int32.TryParse(sideA, out sideNameA))
+            {
+                if (Int32.TryParse(sideB, out sideNameB))
+                {
+                    if (sideNameA <= 0 || sideNameB <= 0)
+                    {
+                        Console.WriteLine("Ошибка, значение не может быть отрицательным или нуль");
+                    }
+                    else
+                    {
+                        area += sideNameA * sideNameB;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("значение b не число !");
+                }
+            }
+            else
+            {
+                Console.WriteLine("значение a не число !");
+            }
+            return area;
         }
     }
 }
