9a7a81082d2478333e968686151df4b647e7d0bd
Fernand Moudio <fernandmoudio1@gmail.com>
2019-09-24 14:05:14 +0300

    task2 done

--- Moudio_Fernand_Task02/Task2/Program.cs
+++ Moudio_Fernand_Task02/Task2/Program.cs
@@ -10,6 +10,55 @@ namespace Task2
     {
         static void Main(string[] args)
         {
+            int number;
+            do
+            {
+                Console.Write("Введите число N : ");
+                string num = Console.ReadLine();                
+                number = ConvertValue(num);
+                CheckValue(number);
+            } while (number <= 0);
+
+            ShowPicture(number);
+        }
+
+        static void ShowPicture(int number)
+        {
+            string start = "*";
+            if (number == 1)
+                Console.WriteLine(start);
+            else
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    Console.WriteLine(start);
+                    start = start + "*";
+                }
+            }
+        }
+
+        static int ConvertValue(string value)
+        {
+            int convertedValue;
+            int myValue = 0;
+            if (Int32.TryParse(value, out convertedValue))
+            {
+                myValue += convertedValue;
+            }
+            else
+            {
+                myValue = -1;
+            }
+            return myValue;
+        }
+
+        static void CheckValue(int value)
+        {
+
+            if (value <= 0)
+            {
+                Console.WriteLine("Ошибка, повторите пожалуйста !");
+            }
         }
     }
 }
