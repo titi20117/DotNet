461a593e2d0f51aa7e3d4701046dd1bcd87a23a4
Fernand Moudio <fernandmoudio1@gmail.com>
2019-09-24 06:55:48 +0300

    make final change in Task1

--- Moudio_Fernand_Task02/Task1/Program.cs
+++ Moudio_Fernand_Task02/Task1/Program.cs
@@ -10,41 +10,65 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.Write("Напишите значению сторону a прямоугольника a = ");
-            string sideA = Console.ReadLine();
-            Console.Write("Напишите значению сторону b прямоугольника b = ");
-            string sideB = Console.ReadLine();
-            int area = FindAreaRectangle(sideA, sideB);
-            Console.WriteLine("Площадь прямоугольника S = " + area);
+            int sideNameA;
+            do
+            {
+                Console.Write("Напишите значению сторону a прямоугольника a = ");
+                string sideA = Console.ReadLine();
+                sideNameA = ConvertValue(sideA);
+                CheckValue(sideNameA);
+            } while (sideNameA == 1);
+
+            int sideNameB;
+            do
+            {
+                Console.Write("Напишите значению сторону a прямоугольника b = ");
+                string sideB = Console.ReadLine();
+                sideNameB = ConvertValue(sideB);
+                CheckValue(sideNameB);
+            } while (sideNameB == 1);
+
+            ResultMessage(sideNameA, sideNameB);
         }
 
-        static int FindAreaRectangle (string sideA, string sideB)
+        static int FindAreaRectangle (int sideA, int sideB)
         {
-            int area = 0;
-            int sideNameA, sideNameB;
-            if (Int32.TryParse(sideA, out sideNameA))
+            return sideA * sideB;
+        }
+
+        static int ConvertValue (string value)
+        {
+            int convertedValue = 0;
+            if (Int32.TryParse(value, out convertedValue))
             {
-                if (Int32.TryParse(sideB, out sideNameB))
-                {
-                    if (sideNameA <= 0 || sideNameB <= 0)
-                    {
-                        Console.WriteLine("Ошибка, значение не может быть отрицательным или нуль");
-                    }
-                    else
-                    {
-                        area += sideNameA * sideNameB;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("значение b не число !");
-                }
+                convertedValue += convertedValue;
             }
             else
             {
-                Console.WriteLine("значение a не число !");
+                convertedValue = 1;
             }
-            return area;
+            return convertedValue;
+        }
+
+        static void CheckValue (int value)
+        {
+            if (value == 1)
+            {
+                Console.WriteLine("значение не число!");
+            }
+        }
+
+        static void ResultMessage (int valueA, int valueB)
+        {
+            if (valueA <= 0 || valueB <= 0)
+            {
+                Console.WriteLine("Ошибка, значение не может быть отрицательным или нуль");
+            }
+            else
+            {
+                int area = FindAreaRectangle(valueA, valueB);
+                Console.WriteLine("Площадь прямоугольника S = " + area);
+            }
         }
     }
 }
