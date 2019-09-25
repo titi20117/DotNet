968799f431cd7222efb754a74f92c5b6ba1e1476
Fernand Moudio <fernandmoudio1@gmail.com>
2019-09-25 04:48:38 +0300

    Task5 done

--- Moudio_Fernand_Task02/Task5/Program.cs
+++ Moudio_Fernand_Task02/Task5/Program.cs
@@ -10,6 +10,21 @@ namespace Task5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("сумма всех чисел меньше 1000 равно : " + FindSum(1000));
+            Console.ReadKey();
+        }
+
+        static int FindSum (int number)
+        {
+            int sum = 0;
+            for (int i = 1; i < number; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
         }
     }
 }
