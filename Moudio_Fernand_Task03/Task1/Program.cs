dfb9f26ba40a149ba802007ff1819aa1d51d06d5
Fernand <moudio.fernand@yandex.ru>
2019-09-25 20:00:16 +0300

    incomplete version

--- Moudio_Fernand_Task03/Task1/Program.cs
+++ Moudio_Fernand_Task03/Task1/Program.cs
@@ -10,6 +10,66 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            int[] arr = new int[20];
+            var rand = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(100);
+            }
+            Console.WriteLine("Maximun : " + FindMaxElement(arr));
+            Console.WriteLine("Minimun : " + FindMinElement(arr));
+            Console.ReadKey();
+
+        }
+
+        static int FindMaxElement(int[] arr)
+        {
+            int max = -1;
+            foreach (int element in arr)
+            {
+                if (element > max)
+                {
+                    max = element;
+                }
+            }
+            return max;
+        }
+
+        static int FindMinElement(int[] arr)
+        {
+            int min = 10000;
+            foreach (int element in arr)
+            {
+                if (element < min)
+                {
+                    min = element;
+                }
+            }
+            return min;
+        }
+
+        static int[] sortArray(int[] arr)
+        {
+            int[] ordArray = new int[arr.Length];
+            int min = 100000;
+            bool test = false;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] == FindMinElement(arr))
+                {
+                    ordArray[i] = arr[i];
+                }
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                    test = true;
+                }
+                else
+                {
+
+                }
+            }
+            
         }
     }
 }
--- Moudio_Fernand_Task03/Task4/Program.cs
+++ Moudio_Fernand_Task03/Task4/Program.cs
@@ -10,41 +10,10 @@ namespace Task4
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[20];
-            var rand = new Random();
-            for (int i = 0; i < arr.Length; i++)
-            {
-                arr[i] = rand.Next(100);
-            }
-            Console.WriteLine("Maximun : " + FindMaxElement(arr));
-            Console.WriteLine("Minimun : " + FindMinElement(arr));
+           
             Console.ReadKey();
         }
 
-        static int FindMaxElement(int[] arr)
-        {
-            int max = -1;
-            foreach (int element in arr)
-            {
-                if (element > max)
-                {
-                    max = element;
-                }
-            }
-            return max;
-        }
-
-        static int FindMinElement(int[] arr)
-        {
-            int min = 10000;
-            foreach (int element in arr)
-            {
-                if (element < min)
-                {
-                    min = element;
-                }
-            }
-            return min;
-        }
+        
     }
 }
