90d1ceb61ffec2f97bff83b94df1bc73633ecf48
Fernand <moudio.fernand@yandex.ru>
2019-09-26 14:25:44 +0300

    Task3 done

--- Moudio_Fernand_Task03/Task3/Program.cs
+++ Moudio_Fernand_Task03/Task3/Program.cs
@@ -10,6 +10,38 @@ namespace Task3
     {
         static void Main(string[] args)
         {
+            int[] array = new int[10];
+            var rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(-50, 50);
+            }
+            Console.Write("Одномерная массива M : ");
+            DisplayArray(array);
+            Console.WriteLine();
+            Console.WriteLine("Cумма неотрицательных элементов S = " + SumNonNegativeElement(array));
+            Console.ReadKey();
+        }
+
+        static int SumNonNegativeElement(int[] array)
+        {
+            int sum = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                if(array[i] >= 0)
+                {
+                    sum += array[i];
+                }
+            }
+            return sum;
+        }
+
+        static void DisplayArray(int[] array)
+        {
+            foreach(int el in array)
+            {
+                Console.Write(el + "    ");
+            }
         }
     }
 }
