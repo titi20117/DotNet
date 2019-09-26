3437dfbe4101fcd91395d22d73c5efdf4e774055
Fernand <moudio.fernand@yandex.ru>
2019-09-26 13:53:46 +0300

    Task2 done

--- Moudio_Fernand_Task03/Task2/Program.cs
+++ Moudio_Fernand_Task03/Task2/Program.cs
@@ -10,6 +10,56 @@ namespace Task2
     {
         static void Main(string[] args)
         {
+            int[,,] array = new int [2,2,3] 
+            {
+                {
+                    { -1, 2, -3 },
+                    { 14, -5, 6 }
+                },
+                { 
+                    { -1, 2, -3 }, 
+                    { 14, -5, 6 } 
+                }
+            };
+            
+            Console.WriteLine("Исходный массив А: ");
+            DisplayArray(array);
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine("результат: ");
+            DisplayArray(ChangePositiveElement(array));
+            Console.ReadKey();
+        }
+
+        static int[,,] ChangePositiveElement(int[,,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    for (int k = 0; k < array.GetLength(2); k++)
+                    {
+                        if (array[i, j, k] > 0)
+                            array[i, j, k] = 0;
+                    }
+                }
+            }
+            return array;
+        }
+
+        static void DisplayArray(int[,,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.WriteLine(".....................................");
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    for (int k = 0; k < array.GetLength(2); k++)
+                    {
+                        Console.Write(array[i, j, k] + "    ");
+                    }
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
