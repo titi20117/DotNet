6df51190ed0799a334fe58ee3f7189df8d919ce4
Fernand Moudio <fernandmoudio1@gmail.com>
2019-09-30 09:55:39 +0300

    start Task3

--- Moudio_Fernand_Task04/Task3/Program.cs
+++ Moudio_Fernand_Task04/Task3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,31 @@ namespace Task3
     {
         static void Main(string[] args)
         {
+            DateTime dateTimeIn = DateTime.Now;
+            Console.WriteLine("Формат отображения даты и времени");
+            Console.WriteLine("ru vs en");
+            GetCultureName("ru-RU", "en-EN");
+            Console.WriteLine("en vs invariant");
+            GetCultureName("en-EN", "InvariantCulture");
+            Console.WriteLine("ru vs invariant");
+            GetCultureName("ru-RU", "InvariantCulture");
+            Console.WriteLine("Формат отображения числовых данных");
+            Console.WriteLine("ru vs en");
+            Console.WriteLine("en vs invariant");
+            Console.WriteLine("ru vs invariant");
+            Console.ReadKey();
+        }
+
+        private static void GetCultureName(string cultureName1, string cultureName2)
+        {
+            DateTime dateTimeIn = DateTime.Now;
+            CultureInfo firstCulture = new CultureInfo(cultureName1);
+            CultureInfo secondCulture = new CultureInfo(cultureName2);
+            Console.WriteLine("Date formatted for the {0} culture: {1}",
+                           firstCulture.Name, dateTimeIn.ToString("f", firstCulture));
+            Console.WriteLine("Date formatted for the {0} culture: {1}",
+                           secondCulture.Name, dateTimeIn.ToString("f", secondCulture));
+            //CultureInfo invC = CultureInfo.InvariantCulture;
         }
     }
 }
