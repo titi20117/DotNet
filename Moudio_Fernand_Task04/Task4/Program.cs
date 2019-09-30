6344569383a262a52f3d572b922ad6c1e0525b37
Fernand <moudio.fernand@yandex.ru>
2019-09-30 16:29:41 +0300

    Task 4 done

--- Moudio_Fernand_Task04/Task4/Program.cs
+++ Moudio_Fernand_Task04/Task4/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StringLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,38 @@ namespace Task4
     {
         static void Main(string[] args)
         {
+            string str = "";
+            StringBuilder sb = new StringBuilder();
+            int N = 100;
+            DateTime startTime = DateTime.Now;
+            for (int i = 0; i < N; i++)
+            {
+                str += "*";
+            }
+            DateTime stopTime = DateTime.Now;
+            Console.WriteLine("start: " + startTime.Millisecond);
+            Console.WriteLine("stop: " + stopTime.Millisecond);
+            Console.WriteLine("скорость работы String : {0} {1}", FindExecutionSpeed(AddStatus(startTime), AddStatus(stopTime)), "ms");
+            startTime = DateTime.Now;
+            for (int i = 0; i < N; i++)
+            {
+                sb.Append("*");
+            }
+            stopTime = DateTime.Now;
+            Console.WriteLine("start: " + startTime.Millisecond);
+            Console.WriteLine("stop: " + stopTime.Millisecond);
+            Console.WriteLine("скорость работы StringBuilder : {0} {1}", FindExecutionSpeed(AddStatus(startTime), AddStatus(stopTime)), "ms");
+            Console.ReadKey();
+        }
 
+        static long FindExecutionSpeed (long start, long stop)
+        {
+            return stop - start;
+        } 
+
+        static long AddStatus(DateTime date)
+        {
+            return date.Millisecond;
         }
     }
 }
