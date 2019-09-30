40677a503c81ce369b846ef45976125a537baa07
Fernand <moudio.fernand@yandex.ru>
2019-10-01 01:56:57 +0300

    Task5 done

--- Moudio_Fernand_Task04/Task5/Program.cs
+++ Moudio_Fernand_Task04/Task5/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Task5
@@ -10,6 +11,28 @@ namespace Task5
     {
         static void Main(string[] args)
         {
+            String text = "<ba>Это</b> текст <i>с</i> <font color=”red”>HTML</font> кодами";
+            DeleteTag(text);
+            Console.ReadKey();
+        }
+
+        static void DeleteTag(string text)
+        {
+            Regex regex = new Regex(@"</?\w+\s*[\^>]*>");
+            MatchCollection match = regex.Matches(text);
+            if (match.Count > 0)
+            {
+                foreach (Match el in match)
+                {
+                    Console.WriteLine(el.Value);
+                    text = text.Replace(el.Value, "_");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Совпадений не найдено !");
+            }
+            Console.WriteLine(text);
         }
     }
 }
