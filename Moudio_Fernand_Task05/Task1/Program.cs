e648314e79972ef613255060bbfb1aa078216685
Fernand <moudio.fernand@yandex.ru>
2019-10-01 18:05:51 +0300

    Solution for Task05 start, Task1 done

---
+++ Moudio_Fernand_Task05/Task1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+
+            Console.Write("Введите фамилию : ");
+            user.firstName = Console.ReadLine();
+
+            Console.Write("Введите имя : ");
+            user.middleName = Console.ReadLine(); ;
+
+            Console.Write("Введите отчество : ");
+            user.lastName = Console.ReadLine();
+
+            Console.Write("Введите дату рождению : ");
+            user.dateOfBirth = Console.ReadLine();
+
+            Console.WriteLine("Проверка");
+            Console.WriteLine("User FIO : {0}", user.GetFullNameUser());
+            Console.WriteLine("User лет : {0}", user.GetAge());
+            Console.ReadKey();
+        }
+
+        public class User
+        {
+            public string firstName { get; set; }
+            public string middleName { get; set; }
+            public string lastName { get; set; }
+            public string dateOfBirth { get; set; }
+            private int age { get { return int.Parse(DateTime.Now.Year.ToString()) - int.Parse(dateOfBirth); } }
+
+            public int GetAge()
+            {
+                return age;
+            }
+            
+            public string GetFullNameUser()
+            {
+                return this.firstName + " " + this.middleName + " " + this.lastName;
+            }
+        }
+    }
+}
