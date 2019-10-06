f69a4206689b6b96eabaa450b0af61d0f6ad7195
Fernand <moudio.fernand@yandex.ru>
2019-10-07 02:41:33 +0300

    Task2 done

--- Moudio_Fernand_Task06/Task2/Program.cs
+++ Moudio_Fernand_Task06/Task2/Program.cs
@@ -10,22 +10,79 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-
+            Ring ring = new Ring(0, 0, 3, 10);
+            Console.WriteLine("Кольцо с координатом О:({0}, {1})\nВнутренниq радиуc r={2}\nВнешний радиус R={3}"
+                , ring.GetX(), ring.GetY(), ring.GetRadius(), ring.GetExternRadius());
+            Console.WriteLine("Периметр кольцо P:= {0}", ring.GetPerim());
+            Console.WriteLine("Площадь кольцо S:= {0}", ring.GetArea());
+            Console.ReadKey();
         }
 
         public class Round
         {
-            private int x;
-            private int y;
-            private int radius;
-            public double perimetr { get { return 2 * Math.PI * radius; } }
-            public double aire { get { return Math.PI * Math.Pow(radius, 2); } }
-            public Round(int X, int Y, int rad)
+            protected int x;
+            protected int y;
+            protected int radius;
+            protected double perimetr { get { return 2 * Math.PI * radius; } }
+            protected double aire { get { return Math.PI * Math.Pow(radius, 2); } }
+            public Round(int X, int Y, int innerRadius)
             {
                 this.x = X;
                 this.y = Y;
-                this.radius = rad;
+                this.radius = innerRadius;
             }
+
+            public int GetX()
+            {
+                return x;
+            }
+            public int GetY()
+            {
+                return y;
+            }
+
+            public int GetRadius()
+            {
+                return radius;
+            }
+
+            public virtual double GetPerim()
+            {
+                return perimetr;
+            }
+
+            public virtual double GetArea()
+            {
+                return aire;
+            }
+        }
+
+        public class Ring : Round
+        {
+            private int secondRadius;
+            private double perimetrExtern { get { return 2 * Math.PI * secondRadius; } }
+            private double aireExtern { get { return Math.PI * Math.Pow(secondRadius, 2); } }
+            public Ring(int X, int Y, int innerRadius, int secondRadius)
+                :base(X, Y, innerRadius)
+            {
+                this.secondRadius = secondRadius;
+            }
+
+            public int GetExternRadius()
+            {
+                return secondRadius;
+            }
+            public override double GetPerim()
+            {
+                return perimetrExtern + base.GetPerim();
+            }
+
+            public override double GetArea()
+            {
+                return aireExtern - base.GetArea();
+
+            }
+
         }
     }
 }
