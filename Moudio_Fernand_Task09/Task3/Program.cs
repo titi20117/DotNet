84e7561255cfd8c3b8d89376ee24811edbf105c6
Fernand <moudio.fernand@yandex.ru>
2019-10-25 06:16:00 +0300

    words split regex

--- Moudio_Fernand_Task09/Task3/Program.cs
+++ Moudio_Fernand_Task09/Task3/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Task3
@@ -10,6 +11,47 @@ namespace Task3
     {
         static void Main(string[] args)
         {
+            string text = "is good to know something new, i like to play a football, go to the training, listen music" +
+                "with my friends";
+            RegexDemo regexDemo = new RegexDemo();
+            regexDemo.Demo();
+            Console.ReadKey();
+        }
+    }
+
+    public class RegexDemo
+    {
+        public void Demo()
+        {
+            Regex regex = new Regex(@"\d+");
+
+            Match match = regex.Match("Dot 55 Perls 777");
+            while (match.Success)
+            {
+                Console.WriteLine(match.Value);
+                match = match.NextMatch();
+            }
+
+            string sentence = "10 cats, 20 dogs, 40 fish and 1 programmer.";
+            string[] digits = Regex.Split(sentence, @"\w+");
+            foreach (string value in digits)
+            {
+                int number;
+                if (int.TryParse(value, out number))
+                {
+                    Console.WriteLine(value);
+                }
+            }
+
+
+            string pattern = @"\b(\w+?)\s\1\b";
+            string input = "This this is a nice day. What about this? This tastes good. I saw a a dog.";
+
+            foreach (Match match1 in Regex.Matches(input, pattern, RegexOptions.IgnoreCase))
+            {
+                Console.WriteLine("{0} (duplicates '{1}') at position {2}",
+                    match1.Value, match1.Groups[1].Value, match1.Index);
+            }
         }
     }
 }
---
+++ Moudio_Fernand_Task09/Task3/Words.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class Words<T> : IEnumerable<string>
+    {
+        private string text;
+        private string[] textToArray;
+        public int CountRepeatingWords{ get { return 5; } }
+        
+        
+        public Words(string someText)
+        {
+            text = someText;
+            //textToArray = text.Split(text, "")
+            for (int i = 0; i < text.Length; i++)
+            {
+
+            }
+        }
+        public IEnumerator<string> GetEnumerator()
+        {
+            for (int i = 0; i < textToArray.Length; i++)
+            {
+                yield return textToArray[i];
+
+                if (i > textToArray.Length)
+                    yield break;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
