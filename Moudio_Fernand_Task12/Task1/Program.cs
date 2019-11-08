fe75d1ee6cd1ca02f0de1bfc3c35454ff53e185e
Fernand Moudio <fernandmoudio1@gmail.com>
2019-11-08 07:52:24 +0300

    Project Task12 done

--- Moudio_Fernand_Task12/Task1/Program.cs
+++ Moudio_Fernand_Task12/Task1/Program.cs
@@ -11,40 +11,51 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\loude\Desktop\Epam2019Materiels\прак12");
-            dir.Create();
+            string path = @"C:\Users\loude\Desktop\Epam2019Materiels\прак12\disposable_task_file.txt";
 
-            string filePath = Path.Combine(dir.FullName, "disposable_task_file.txt");
-            string filePath2 = Path.Combine(dir.FullName, "disposable_task_file2.txt");
-            UTF8Encoding encoding = new UTF8Encoding();
-            //File.ReadAllLines()
-            //File.WriteAllLines()
+            string[] readText = File.ReadAllLines(path);
+            ReadFile(readText);
 
-            using(FileStream fs = File.Open(filePath, FileMode.Open, FileAccess.ReadWrite, FileShare.None))
-            {
-                byte[] buffer = new byte[fs.Length];
-                while(fs.Read(buffer, 0, buffer.Length) > 0)
-                {
-                    string fileContent = encoding.GetString(buffer);
-                    Console.WriteLine(fileContent);
-                    char[] delimitedChars = { '\t', '\n' };
-                    string[] tab = fileContent.Split(delimitedChars);
-                    //for (int i = 0; i < tab.Length; i++)
-                    //{
-                    //    int num = Int32.Parse(tab[i]);
-                    //    tab[i] = (num * num).ToString();
-                    //}
-                    foreach (var el in tab)
-                    {
-                        int num = Int32.Parse(el);
-                        Console.WriteLine(num * num);
+            string[] arr = new string[readText.Length];
+            arr = SetSquareValueInFile(readText);
 
-                    }
-                    //byte[] info = encoding.GetBytes(tab);
-                    //fs.Write(info, 0, info.Length);
-                }
-            }
+            ModifyFile(path, arr);
+            
             Console.ReadKey();
+        }
+
+        static string[] SetSquareValueInFile(string[] readText)
+        {
+            string[] arr = new string[readText.Length];
+            for (int i = 0; i < readText.Length; i++)
+            {
+                int num = Int32.Parse(readText[i]);
+                int numSquare = num * num;
+                arr[i] = numSquare.ToString();
+            }
+            return arr;
+        }
+
+        static void ModifyFile(string path, string[] arr)
+        {
+            string[] createText = arr;
+            if (!File.Exists(path))
+            {
+                File.WriteAllLines(path, createText);
+            }
+            else
+            {
+                File.Delete(path);
+                File.WriteAllLines(path, createText);
+            }
+        }
+
+        static void ReadFile(string[] readText)
+        {
+            foreach (string s in readText)
+            {
+                Console.WriteLine(s);
+            }
         }
     }
 }
--- Moudio_Fernand_Task12/Task2/Program.cs
+++ Moudio_Fernand_Task12/Task2/Program.cs
@@ -11,7 +11,70 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Выберите режим работы программы\r\n1.Режим наблюдателя\r\n2.Режим отката\r\nДля выхода из программы нажмите любую кнопку");
+            if (Console.Read() == '1')
+            {
+                Observer();
+                Console.ReadKey();
+            }
+            if (Console.Read() == '2')
+            {
+                Console.WriteLine("В разработке");
+                Console.ReadKey();
+            }
+        }
+        private static string path = "C:\\Exchange";
+        private static string pathSystem = "C:\\ExchangeSystem";
+        private static FileSystemWatcher watcher = new FileSystemWatcher();
+        public static void Observer()
+        {
+            Console.WriteLine("Включён режим наблюдателя");
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = path;
+            watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.LastAccess | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.EnableRaisingEvents = true;
+            watcher.Filter = "*.*";
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnDelete);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+            watcher.Changed += new FileSystemEventHandler(Changed);
+
+            watcher.EnableRaisingEvents = true;
+            Console.WriteLine("Нажмите q для выхода из программы.");
+        }
+        private static void Changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Произошло изменение файла {e.Name} \r\nДействие произведенное с ним: {e.ChangeType} \r\nПуть до файла: {e.FullPath}");
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Новый файл: {e.Name} \r\nДействие произведенное с ним: {e.ChangeType} \r\nПуть до файла: {e.FullPath} \r\n");
+            CopyDir(path, pathSystem);
+        }
+        private static void OnDelete(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Удален файл: {e.Name}\r\nДействие произведенное с ним: {e.ChangeType}\r\nУдален файл из: {e.FullPath}\r\n");
+            CopyDir(path, pathSystem);
+        }
+        private static void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine($"File {e.OldFullPath} renamed to {e.FullPath}\r\n");
+            CopyDir(path, pathSystem);
+        }
+
+        static void CopyDir(string FromDir, string ToDir)
+        {
+            Directory.CreateDirectory(ToDir);
+            foreach (string s1 in Directory.GetFiles(FromDir))
+            {
+                string s2 = ToDir + "\\" + Path.GetFileName(s1);
+                File.Copy(s1, s2);
+            }
+            foreach (string s in Directory.GetDirectories(FromDir))
+            {
+                CopyDir(s, ToDir + "\\" + Path.GetFileName(s));
+            }
         }
     }
 }
