71f8f013b955ab73e5488e760a66eb949c79d5a5
Fernand <moudio.fernand@yandex.ru>
2019-12-06 03:59:16 +0300

    Part2 dotNet

---
+++ Moudio_Fernand_Task15/Entities/Awards.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities
+{
+    public class Awards
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
