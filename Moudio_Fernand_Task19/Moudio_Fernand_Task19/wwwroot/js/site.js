dc3cb878a54b34f0b292f35279617716cb589a7b
Fernand Elisee Moudio Moudio <fernandmoudio1@gmail.com>
2020-02-12 22:53:03 +0300

    style user

--- Moudio_Fernand_Task19/Moudio_Fernand_Task19/wwwroot/js/site.js
+++ Moudio_Fernand_Task19/Moudio_Fernand_Task19/wwwroot/js/site.js
@@ -2,6 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-let modalUser = document.getElementById("");
+let modalUser = document.getElementById("newUserModal");
 
 let btnAddUser = document.getElementById("");
+
+let myModal = document.querySelectorAll(".modal");
+let btnClose = document.querySelectorAll(".close");
