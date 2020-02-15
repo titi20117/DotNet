5bdada6002256e09d7e2daf1983adc96a29ab8c4
Fernand Elisee Moudio Moudio <fernandmoudio1@gmail.com>
2020-02-15 14:02:44 +0300

    datetime

--- Moudio_Fernand_Task19/Moudio_Fernand_Task19/Models/UsersAndAwardsViewModel.cs
+++ Moudio_Fernand_Task19/Moudio_Fernand_Task19/Models/UsersAndAwardsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,9 @@ namespace Moudio_Fernand_Task19.Models
     {
         public List<Users> ListUsers { get; set; }
         public List<Awards> ListAwards { get; set; }
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DateBirthday { get; set; }
         public Users NewUser { get; set; }
         public Awards NewAward { get; set; }
         public UsersAndAwardsViewModel()
